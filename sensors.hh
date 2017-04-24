b7906e34f6305411145febfc961500729d9568ea
Matt Langford <matthew.langford95@gmail.com>
2017-04-23 21:30:00 -0400

    TYPES: add types file and remove it from other headers

--- sensors.hh
+++ sensors.hh
@@ -1,6 +1,7 @@
 #pragma once
 #include <Eigen/Dense>
-#include "ukf.hh"
+
+#include "types.hh"
 
 namespace ukf
 {
---
+++ types.hh
@@ -0,0 +1,146 @@
+#pragma once
+#include <iostream>
+#include <Eigen/Dense>
+#include <chrono>
+
+#include "utils.hh"
+
+// TODO: Move function implementations over to the cc file
+
+namespace ukf
+{
+
+//
+// Some basic typedefs not really related to the filter
+//
+using Clock     = std::chrono::high_resolution_clock;
+using Timestamp = Clock::time_point;
+
+//
+// enum for indexing covariance matrix, these don't really apply
+// to the main State type since it's a struct
+//
+enum states : uint8_t
+{
+    X,
+    Y,
+    Z,
+    VX,
+    VY,
+    VZ,
+    RX,
+    RY,
+    RZ,
+    WX,
+    WY,
+    WZ,
+    AX_b,  // meas_a = R * (real_a - bias)
+    AY_b,
+    AZ_b,
+    GX_b,  // TODO
+    GY_b,
+    GZ_b,
+
+    NUM_STATES
+};
+
+//
+// List of sensors
+//   accelerometer -> acceleration | roll | pitch
+//   camera        -> velocity | angular velocity
+//   gyro          -> angular velocity
+//   magnetometer  -> yaw
+//
+
+//
+// Plan for acc bias:
+//   measured_a = R * (real_a - bias)
+//   // assuming real_a = 1g
+//   measured_a = R * ([0, 0, 1] - bias)
+//   bias = -R_inv * measured_a + [0, 0, 1]
+//
+
+// This is an alternate way to keep track of the state...
+struct State
+{
+    Eigen::Vector3d    position;  // meters
+    Eigen::Vector3d    velocity;  // meters per second
+    Eigen::Matrix3d    orientation;  // temporarily a rotation matrix
+    Eigen::Vector3d    angular_vel;  // rads per second
+    Eigen::Vector3d    acc_bias;  // meters per second^2
+    Eigen::Vector3d    gyro_bias;  // rads per second
+
+    State()
+    {
+        position = Eigen::Vector3d::Zero();
+        velocity = Eigen::Vector3d::Zero();
+        orientation = Eigen::Matrix3d::Identity();
+        angular_vel = Eigen::Vector3d::Zero();
+        acc_bias = Eigen::Vector3d::Zero();
+        gyro_bias = Eigen::Vector3d::Zero();
+    }
+
+    // makes the sigma point transformation pretty easy
+    State operator+(const Eigen::Matrix<double, states::NUM_STATES, 1>& lhs) const
+    {
+        State new_state;
+        Eigen::Vector3d lie_rotation(ln(orientation));      
+        new_state.position = position + lhs.block<3, 1>(states::X, 0);
+        new_state.velocity = velocity + lhs.block<3, 1>(states::VX, 0);
+        new_state.orientation = exp(lie_rotation + lhs.block<3, 1>(states::RX, 0));
+        new_state.angular_vel = angular_vel + lhs.block<3, 1>(states::WX, 0);
+        new_state.acc_bias = acc_bias + lhs.block<3, 1>(states::AX_b, 0);
+        new_state.gyro_bias = gyro_bias + lhs.block<3, 1>(states::GX_b, 0);
+
+        return new_state;
+    };
+
+    void print() const
+    {
+        std::cout << "position:           " << position.transpose() << std::endl;
+        std::cout << "velocity:           " << velocity.transpose() << std::endl;
+        std::cout << "orientation (x vec) " << orientation.block<3, 1>(0, 0).transpose()<< std::endl;
+        std::cout << "angular_vel:        " << angular_vel.transpose()<< std::endl;
+        std::cout << "acc_bias:           " << acc_bias.transpose() << std::endl;
+        std::cout << "gyro_bias:          " << gyro_bias.transpose() << std::endl;
+    }
+};
+
+//
+// Covariance matrix, use the states enum to index this
+//
+using Covariance = Eigen::Matrix<double, states::NUM_STATES, states::NUM_STATES>;
+//
+// Computed during the UT, a list of 2 * states::NUM_STATES + 1 State's that
+// are passed into the transition function for each UT. The transition function
+// should return a new set of SigmaPoints.
+//
+using SigmaPoints = std::vector<State>;
+//
+// Intertial sensors are used using a time update
+//
+
+
+//
+// There should be 2*n + 1 sigma points, we can pre-compute it here
+//
+constexpr size_t NUM_SIGMA_POINTS = 2 * states::NUM_STATES + 1;
+
+//
+// Structs returned from and used by the UKF
+//
+struct StateAndCovariance
+{
+    State      state;
+    Covariance covariance;
+
+    void print() const
+    {
+        std::cout << "State: " << std::endl;
+        state.print();
+        std::cout << "Covariance: " << std::endl;
+        std::cout << covariance << std::endl;
+    }
+};
+
+}  // namespace
